727146fb2a0a5d87b5ac4557c5c73890decb225e
Noah Davis <noahd1@yahoo.com>
2015-06-09 21:08:30 -0400

    Put compound conditional on one line

--- lib/minidoc/duplicate_key.rb
+++ lib/minidoc/duplicate_key.rb
@@ -2,8 +2,7 @@ class Minidoc::DuplicateKey < Mongo::OperationFailure
   DUPLICATE_KEY_ERROR_CODE = 11000
 
   def self.duplicate_key_exception(exception)
-    if exception.respond_to?(:error_code) &&
-      exception.error_code == DUPLICATE_KEY_ERROR_CODE
+    if exception.respond_to?(:error_code) && exception.error_code == DUPLICATE_KEY_ERROR_CODE
       new(exception.message, exception.error_code, exception.result)
     else
       nil

22ce8210e07769de549001c8b9e5b4fd7942d8b9
Will Fleming <will@flemi.ng>
2016-09-27 17:58:09 -0400

    Infer association class name when not provided (#32)
    * Infer association class name when not provided
    
    Defining setters & getters blindly without knowing if a class name was
    provided doesn't seem great. Needing to provide a class name explicitly
    when in many cases it is easily inferrable also seems odd.
    
    This does a simple inferring of the class name for an association based
    on the association name.
    
    * Infer class name in current module

--- test/belongs_to_test.rb
+++ test/belongs_to_test.rb
@@ -6,7 +6,22 @@ class BelongsToTest < Minidoc::TestCase
     belongs_to :owner, class_name: "User"
   end
 
+  class ::Dog < Minidoc
+    include Minidoc::Associations
+    belongs_to :user
+  end
+
   class User < ::User
+  end
+
+  module Animal
+    class Armadillo < Minidoc
+      include Minidoc::Associations
+      belongs_to :predator
+    end
+
+    class Predator < Minidoc
+    end
   end
 
   def test_loading
@@ -45,5 +60,23 @@ class BelongsToTest < Minidoc::TestCase
     assert_equal "Bryan", cat.owner.name # doesn't change
     cat.owner_id = user.id
     assert_equal "Noah", cat.owner.name # changes
+  end
+
+  def test_top_level_inferred_class_name
+    assert_nil Dog.new.user
+    user = User.create
+    sam = Dog.new(user_id: user.id)
+    assert_equal user.id, sam.user.id
+    sam.save
+    assert_equal user.id, sam.user.id
+  end
+
+  def test_nested_inferred_class_name
+    assert_nil Animal::Armadillo.new.predator
+    predator = Animal::Predator.create
+    arnie = Animal::Armadillo.new(predator_id: predator.id)
+    assert_equal predator.id, arnie.predator.id
+    arnie.save
+    assert_equal predator.id, arnie.predator.id
   end
 end

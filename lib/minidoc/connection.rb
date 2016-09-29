0ad0d1ef15609e634d5201de073c63e522ced713
Max Jacobson <max@hardscrabble.net>
2016-09-29 10:53:25 -0400

    Raise better error when configuration is missing

--- lib/minidoc/connection.rb
+++ lib/minidoc/connection.rb
@@ -8,12 +8,15 @@ module Minidoc::Connection
     class_attribute :database_name
   end
 
+  MissingConfiguration = Class.new(StandardError)
+
   module ClassMethods
     def collection
       database[collection_name]
     end
 
     def database
+      validate_config
       connection[database_name]
     end
 
@@ -23,6 +26,16 @@ module Minidoc::Connection
 
     def collection_name
       @collection_name ||= name.demodulize.underscore.pluralize
+    end
+
+    private
+
+    def validate_config
+      if connection.nil?
+        raise MissingConfiguration, "Make sure to set Minidoc.connection"
+      elsif database_name.nil?
+        raise MissingConfiguration, "Make sure to set Minidoc.database_name"
+      end
     end
   end
 end

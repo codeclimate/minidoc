337fc8bb107060a2467e3d88b2fc2e1318df6d74
Will Fleming <will@flemi.ng>
2017-10-24 13:00:26 -0400

    Fix Minidoc::ReadOnly connection inheritance
    The previous implementation effectively presumed all minidoc subclasses
    are using one connection/database, which we aren't doing anymore.
    
    This fixes these methods to support overriding these attributes on read
    only classes, same as normal Minidoc clases.

--- lib/minidoc/read_only.rb
+++ lib/minidoc/read_only.rb
@@ -5,7 +5,14 @@ class Minidoc::ReadOnly < Minidoc::Value
   include Minidoc::Connection
   include Minidoc::Finders
 
-  def self.database
-    Minidoc.database
+  # This and `self.database_name` are deleted & redefined by `class_attribute`
+  # when you call `.connection=` or `.database_name=`, so these still allow
+  # `ReadOnly` subclasses to override their connection details
+  def self.connection
+    Minidoc.connection
+  end
+
+  def self.database_name
+    Minidoc.database_name
   end
 end

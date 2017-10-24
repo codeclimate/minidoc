6dc7449c29a55d1a54690a82295e2e67785899e9
Max Jacobson <max@hardscrabble.net>
2017-10-24 15:11:54 -0400

    Demo

--- lib/minidoc/read_only.rb
+++ lib/minidoc/read_only.rb
@@ -9,11 +9,15 @@ class Minidoc::ReadOnly < Minidoc::Value
     super
   rescue MissingConfiguration, NoMethodError
     Minidoc.connection
+  else
+    raise "hell"
   end
 
   def self.database_name
     super
   rescue MissingConfiguration, NoMethodError
     Minidoc.database_name
+  else
+    raise "hell"
   end
 end

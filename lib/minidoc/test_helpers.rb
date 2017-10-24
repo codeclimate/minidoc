a7252183b4f0457bf51c81ad71d6d814e233bf4d
Ale Paredes <alexandra@codeclimate.com>
2017-10-24 18:36:03 -0400

    Allow to clear any database

--- lib/minidoc/test_helpers.rb
+++ lib/minidoc/test_helpers.rb
@@ -2,25 +2,27 @@ class Minidoc
   module TestHelpers
     extend self
 
-    def clear_database
-      clear_collections
-      clear_indexes
+    def clear_databases(connections)
+      connections.each { |connection| clear_database(connection) }
     end
 
-    def clear_collections
-      each_collection { |c| c.remove({}) }
+    def clear_database(connection = Minidoc.connection)
+      clear_collections(connection)
+      clear_indexes(connection)
     end
 
-    def clear_indexes
-      each_collection(&:drop_indexes)
+    def clear_collections(connection = Minidoc.connection)
+      each_collection(connection) { |c| c.remove({}) }
     end
 
-    def each_collection(&block)
-      [$mongo, $alt_mongo].each do |conn|
-        conn.db.collections.
-          reject { |c| c.name.include?("system") }.
-          each(&block)
-      end
+    def clear_indexes(connection = Minidoc.connection)
+      each_collection(connection, &:drop_indexes)
+    end
+
+    def each_collection(connection, &block)
+      connection.db.collections.
+        reject { |c| c.name.include?("system") }.
+        each(&block)
     end
   end
 end

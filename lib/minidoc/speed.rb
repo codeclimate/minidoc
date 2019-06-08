2d28b2fc39efe21cf4513ccde7135e71a7ea5bf9
cmcommand <45670615+cmcommand@users.noreply.github.com>
2019-06-08 06:31:51 -0700

    Speed.rb

---
+++ lib/minidoc/speed.rb
@@ -0,0 +1,18 @@
+npm test
+class speed
+
+speed cubeTurn.({5})
+speed cubeScrable.({100})
+speed cubeCornerCut.({5})
+
+
+import img
+module img
+ else
+ if toFast.report
+  doc cube.ifReported.add({})
+end
+ end
+Elsif
+ class file
+ file import.file.3x3/cube
